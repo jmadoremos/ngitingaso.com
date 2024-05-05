6c289640cf686facfa77401f74be07d4d1dd1ce0
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2024-05-05 06:21:21 +0000

    Add GitHub pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jmadoremos.github.io/www.ngitingaso.com', // GitHub pages URL
+  base: 'www.ngitingaso.com', // repository name
+});
